d79c2d7b6fe464a828726298e27dfb2c43c2cd29
kalet123-commit <u202310210@upc.edu.pe>
2026-05-07 18:04:04 -0500

    develop

---
+++ .env.ps1
@@ -0,0 +1,22 @@
+﻿$env:SPRING_PROFILES_ACTIVE="dev"
+
+# Database
+$env:DB_HOST="localhost"
+$env:DB_PORT="3306"
+$env:DB_DATABASE="demy"
+$env:DB_USERNAME="demy"
+$env:DB_PASSWORD="demy123"
+
+# JWT
+$env:JWT_SECRET="WW91U2hvdWxkUmVwbGFjZVRoaXNTZWNyZXRJbnByb2R1Y3Rpb24xMjM0NTY3ODkw"
+$env:JWT_EXPIRATION_DAYS="7"
+
+# Email (dummy)
+$env:EMAIL_USER="test@example.com"
+$env:EMAIL_PASS="dummy"
+$env:EMAIL_FROM_ADDRESS="no-reply@demy.com"
+$env:EMAIL_FROM_NAME="Demy"
+
+# Documentation
+$env:DOCUMENTATION_CONTACT_NAME="Demy Team"
+$env:DOCUMENTATION_CONTACT_EMAIL="contact@demy.com"
